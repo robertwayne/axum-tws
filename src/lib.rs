59affd24bd7fbd17205ca42b7aa60418fc802050
Rob Wagner <rob@sombia.com>
2024-02-23 18:39:04 -0500

    Clean up error handling

--- src/lib.rs
+++ src/lib.rs
@@ -28,16 +28,61 @@ pub enum WebSocketError {
 
 impl Display for WebSocketError {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "WebSocket error: {}", self)
+        match self {
+            WebSocketError::ConnectionNotUpgradeable => {
+                write!(f, "connection is not upgradeable")
+            }
+            WebSocketError::Internal(e) => {
+                write!(f, "internal server error: {}", e)
+            }
+            WebSocketError::InvalidConnectionHeader => {
+                write!(f, "invalid `Connection` header")
+            }
+            WebSocketError::InvalidUpgradeHeader => {
+                write!(f, "invalid `Upgrade` header")
+            }
+            WebSocketError::InvalidWebSocketVersionHeader => {
+                write!(f, "invalid `Sec-WebSocket-Version` header")
+            }
+            WebSocketError::MethodNotGet => {
+                write!(f, "http request method must be `GET`")
+            }
+            WebSocketError::UpgradeFailed(e) => {
+                write!(f, "upgrade failed: {}", e)
+            }
+        }
     }
 }
 
-impl std::error::Error for WebSocketError {}
+impl std::error::Error for WebSocketError {
+    fn source(&self) -> Option<&(dyn std::error::Error + 'static)> {
+        match self {
+            WebSocketError::Internal(e) => Some(e),
+            WebSocketError::UpgradeFailed(e) => Some(e),
+            _ => None,
+        }
+    }
+}
 
 impl IntoResponse for WebSocketError {
     fn into_response(self) -> Response<Body> {
+        let status = match self {
+            WebSocketError::ConnectionNotUpgradeable => StatusCode::UPGRADE_REQUIRED,
+
+            // Request headers are invalid or missing.
+            WebSocketError::InvalidConnectionHeader
+            | WebSocketError::InvalidUpgradeHeader
+            | WebSocketError::InvalidWebSocketVersionHeader => StatusCode::BAD_REQUEST,
+
+            // Invalid request method.
+            WebSocketError::MethodNotGet => StatusCode::METHOD_NOT_ALLOWED,
+
+            // All other errors will be treated as internal server errors.
+            _ => StatusCode::INTERNAL_SERVER_ERROR,
+        };
+
         Response::builder()
-            .status(StatusCode::INTERNAL_SERVER_ERROR)
+            .status(status)
             .body(Body::empty())
             .unwrap()
     }
@@ -46,5 +91,11 @@ impl IntoResponse for WebSocketError {
 impl From<tokio_websockets::Error> for WebSocketError {
     fn from(e: tokio_websockets::Error) -> Self {
         WebSocketError::Internal(e)
+    }
+}
+
+impl From<hyper::Error> for WebSocketError {
+    fn from(e: hyper::Error) -> Self {
+        WebSocketError::UpgradeFailed(e)
     }
 }
