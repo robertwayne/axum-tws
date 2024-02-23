f05e1e2e6499bc557a631883ab3b0ce7f2135460
Rob Wagner <rob@sombia.com>
2024-02-23 18:24:56 -0500

    Comment on the magic string used for signing

--- src/upgrade.rs
+++ src/upgrade.rs
@@ -175,6 +175,7 @@ fn sign(key: &[u8]) -> HeaderValue {
 
     let mut sha1 = sha1::Sha1::default();
     sha1.update(key);
+    // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Sec-WebSocket-Accept
     sha1.update(&b"258EAFA5-E914-47DA-95CA-C5AB0DC85B11"[..]);
     let b64 = bytes::Bytes::from(base64::engine::general_purpose::STANDARD.encode(sha1.finalize()));
     HeaderValue::from_maybe_shared(b64).expect("base64 is a valid value")
